df633556fb32b030178ecb050970992321fc9ecc
AlvCarballo <alv.carballo@outlook.es>
2020-01-10 09:06:49 +0100

    first commit

---
+++ src/es/Studium/ejercicio4Tema3/Inicializar/Inicializar.java
@@ -0,0 +1,22 @@
+package es.Studium.ejercicio4Tema3.Inicializar;
+
+
+public class Inicializar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int c=3;
+		int i;
+		int tabla[] = new int[c];
+		for(i=0;i<c;i++)
+		{
+		tabla[i] = 0;
+		}
+		for(i=0;i<c;i++)
+		{
+			System.out.print("Tabla["+i+"]="+tabla[i]+" ");
+		}
+		// Recorrido para mostrar los valores de la tabla 
+		
+	}
+}
